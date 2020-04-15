bc05ac461f9d9dcb0c969e28526a7a669aaa0a15
Ivory Aveline Haskell <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-04-15 13:47:43 +0900

    Update
    Removed Old Firmware And Add Description

--- firmware/Hikaru/src/main.cpp
+++ firmware/Hikaru/src/main.cpp
@@ -4,7 +4,22 @@
 #include "ymz294.h"
 #include "vals.h"
 
+//  _ _ _    _ _ _                   _ _                                                                                     
+// ( | ) |  | (_) |                 ( | )                                                                                    
+//  V V| |__| |_| | ____ _ _ __ _   _V V                                                                                     
+//     |  __  | | |/ / _` | '__| | | |                                                                                       
+//     | |  | | |   < (_| | |  | |_| |                                                                                       
+//  ___|_|_ |_|_|_|\_\__,_|_|__ \__,_|                                          __   _ _ _    _ _ _______ ___  __  __ __ _ _ 
+// |__   __| |          |  ____(_)                                             / _| ( | ) |  | (_)__   __/ _ \|  \/  /_ ( | )
+//    | |  | |__   ___  | |__   _ _ __ _ __ _____      ____ _ _ __ ___    ___ | |_   V V| |__| |_   | | | | | | \  / || |V V 
+//    | |  | '_ \ / _ \ |  __| | | '__| '_ ` _ \ \ /\ / / _` | '__/ _ \  / _ \|  _|     |  __  | |  | | | | | | |\/| || |    
+//    | |  | | | |  __/ | |    | | |  | | | | | \ V  V / (_| | | |  __/ | (_) | |       | |  | | |  | | | |_| | |  | || |    
+//    |_|  |_| |_|\___| |_|    |_|_|  |_| |_| |_|\_/\_/ \__,_|_|  \___|  \___/|_|       |_|  |_|_|  |_|  \___/|_|  |_||_|    
+//    Version 1.56-A
+//    Written by Ivory Aveline Haskell
+//    15/04/2020
 //-------------------------
+
 hc595 shiftRegister(PB_5,PB_4,PB_3,PA_12);
 ymz294 PSG1(shiftRegister,PB_0,PF_1,PA_8);
 ymz294 PSG2(shiftRegister,PB_7,PF_1,PA_8);
@@ -12,8 +27,9 @@ ym2413 FM1(shiftRegister,PB_6,PF_1,PA_8);
 ym2413 FM2(shiftRegister,PB_1,PF_1,PA_8);
 ym2413 FM3(shiftRegister,PF_0,PF_1,PA_8);
 Serial midi(PA_9,PA_10,31250);
-
+Serial pc(USBTX,USBRX,9600);
 //-------------------------
+
 _PSG status_PSG1;
 _PSG status_PSG2;
 _FM  status_FM1;
@@ -21,14 +37,59 @@ _FM  status_FM2;
 _FM  status_FM3;
 _MIDI status_MIDI;
 byte drum = 0x20;
+
 //-------------------------
 
-//ノートオン
+//@brief  :ドラム用レジスタにおける対象のビット位置を返す
+//@param  :ノートナンバー(data...int)
+//@return :ビット位置(int)
+int retDrums(int data){
+  switch(data){
+    case 0x24:
+      //BASS DRUM
+      return 4;
+      break;
+    case 0x26:
+      //SNARE DRUM
+      return 3;
+      break;
+    case 0x2F:
+      //TOM TOM
+      return 2;
+      break;
+    case 0x31:
+      //TOP CYMBAL
+      return 1;
+      break;
+    case 0x2C:
+      //HI-HAT
+      return 0;
+      break;
+    case 0x2E:
+      //HI_HAT
+      return 0;
+      break;
+    default:
+      break;
+    }
+    return -1;
+}
+
+//@brief  :ノートオンに対する処理
+//@param  :対象チャンネル(ch...int)
+//@param  :ノートナンバー(note...int)
+//@return :なし(void)
 void noteOn(int ch,int note){
   if(ch < 15 && ch != 9){
+    //FMメロディー音源の担当チャンネルは1~9,11~14
+    //定数テーブルからFNumberとオクターブを求める
     int f_number = f_numbers[note];
     int oct      = octs[note];
+
+    //空きスロットを探す
+    //見つかった場合、状態を使用中にした上で、該当チャンネルの情報に置き換え発音させる
     for(int i = 0;i < 9;i++){
+      //FM.1
       if(status_FM1.used[i] == false){
         status_FM1.used[i] = true;
         status_FM1.note[i] = note;
@@ -37,6 +98,7 @@ void noteOn(int ch,int note){
       }
     }
     for(int i = 0;i < 9;i++){
+      //FM.2
       if(status_FM2.used[i] == false){
         status_FM2.used[i] = true;
         status_FM2.note[i] = note;
@@ -45,6 +107,7 @@ void noteOn(int ch,int note){
       }
     }
     for(int i = 0;i < 6;i++){
+      //FM.3
       if(status_FM3.used[i] == false){
         status_FM3.used[i] = true;
         status_FM3.note[i] = note;
@@ -54,36 +117,13 @@ void noteOn(int ch,int note){
     }
     return;
   } else if(ch == 9){
-    switch(note){
-      case 0x24:
-        //BASS DRUM
-        drum |= (1 << 4);
-        break;
-      case 0x26:
-        //SNARE DRUM
-        drum |= (1 << 3);
-        break;
-      case 0x2F:
-        //TOM TOM
-        drum |= (1 << 2);
-        break;
-      case 0x31:
-        //TOP CYMBAL
-        drum |= (1 << 1);
-        break;
-      case 0x2C:
-        //HI-HAT
-        drum |= 1;
-        break;
-      case 0x2E:
-        //HI_HAT
-        drum |= 1;
-        break;
-      default:
-        break;
+    //ドラムパートの処理
+    if(retDrums(note) != -1){
+      drum |= (1 << retDrums(note));
+      FM3.setDrums(drum);
     }
-    FM3.setDrums(drum);
   } else {
+    //PSG担当ch(15,16)の処理
     int freq = freqs[note];
     for(int i = 0;i < 3;i++){
       if(status_PSG1.used[i] == false){
@@ -107,8 +147,13 @@ void noteOn(int ch,int note){
 
 }
 
-//ノートオフ
+//@brief  :ノートオフに対する処理
+//@param  :対象チャンネル(ch...int)
+//@param  :ノートナンバー(note...int)
+//@return :なし(void)
 void noteOff(int ch,int note){
+  //ほぼnoteOnと同じであるため説明はそちらを参照
+  //探す際の条件が「使用中 && 対象チャンネルである && 対象のノート番号」となっていること以外は同じである
   if(ch < 15 && ch != 9){
     int f_number = f_numbers[note];
     int oct      = octs[note];
@@ -138,35 +183,10 @@ void noteOff(int ch,int note){
     }
     return;
   } else if(ch == 9){
-    switch(note){
-      case 0x24:
-        //BASS DRUM
-        drum &= ~(1 << 4);
-        break;
-      case 0x26:
-        //SNARE DRUM
-        drum &= ~(1 << 3);
-        break;
-      case 0x2F:
-        //TOM TOM
-        drum &= ~(1 << 2);
-        break;
-      case 0x31:
-        //TOP CYMBAL
-        drum &= ~(1 << 1);
-        break;
-      case 0x2C:
-        //HI-HAT
-        drum &= ~1;
-        break;
-      case 0x2E:
-        //HI_HAT
-        drum &= ~1;
-        break;
-      default:
-        break;
+    if(retDrums(note) != -1){
+      drum &= ~(1 << retDrums(note));
+      FM3.setDrums(drum);
     }
-    FM3.setDrums(drum);
   } else {
     for(int i = 0;i < 3;i++){
       if(status_PSG1.used[i] == true && status_PSG1.ch[i] == true && status_PSG1.note[i] == note){
@@ -183,9 +203,14 @@ void noteOff(int ch,int note){
   }
 }
 
-//コントロールチェンジ
+//@brief  :コントロールチェンジの処理
+//@param  :対象チャンネル(ch...int)
+//@param  :コントロール番号(number...int)
+//@param  :データ(data...byte)
+//@return :なし(void)
 void controlChange(int ch,int number,byte data)
 {
+  //現状エクスプレッションとチャンネルボリュームしか実装していない。
   if(ch < 15){
     if(number == 0x07 || number == 0x0B){
       status_MIDI.exp[ch] = (data >> 3);
@@ -233,8 +258,12 @@ void controlChange(int ch,int number,byte data)
 
 }
 
-//プログラムチェンジ
+//@brief  :プログラムチェンジの処理尾
+//@param  :対象チャンネル(ch...int)
+//@param  :プログラム番号(number...int)
+//@return :なし(void)
 void programmeChange(int ch,int number){
+  //言わずもがなFM専用である。
   status_MIDI.inst[ch] = insts[number];
   for(int i = 0;i < 9;i++){
     if(status_FM1.ch[i] == ch){
@@ -259,12 +288,15 @@ void programmeChange(int ch,int number){
 
 //-------------------------
 
-//全ICのリセット
+//@brief  :リセット
+//@param  :なし(void)
+//@return :なし(void)
 void resetAll(void){
   PSG1.sendZero();
   PSG2.sendZero();
   FM1.reset();
   thread_sleep_for(100);
+  //リズム音源のセットアップ
   FM3.setRegister(0x16,0x20);
   FM3.setRegister(0x17,0x50);
   FM3.setRegister(0x18,0xC0);
@@ -274,15 +306,18 @@ void resetAll(void){
   return;
 }
 
-//MIDIの解析
+//@brief  :MIDIのパース
+//@param  :なし(void)
+//@return :なし(void)
 void midiParse(void){
+  //データを読んで適切な関数に適切な値を渡す
   while(!midi.readable());
   byte data1  = midi.getc();
   byte data2  = 0xFF;
   byte data3  = 0xFF;
+  //命令部分と対象チャンネルを分ける
   byte op     = data1 >> 4;
   byte ch     = data1 ^ (op << 4);
-  
   switch(op){
     case 0x08:
       while(!midi.readable());
--- firmware/Hikaru/src/ymz294.cpp
+++ firmware/Hikaru/src/ymz294.cpp
@@ -1,16 +1,24 @@
 #include <ymz294.h>
 
+//@brief  :コンストラクタ
+//@param  :シフトレジスタ(shiftRegister...hc595)
+//@param  :各ピン(wrcs,ic,a0...PinName)
+//@return :なし(void)
 ymz294::ymz294(hc595 shiftRegister,PinName wrcs,PinName ic,PinName a0) : _shiftRegister(shiftRegister), _wrcs(wrcs), _ic(ic), _a0(a0){   
     _wrcs = 1;
 }
 
-//レジスタのセット
+//@brief  :指定アドレスに指定データを書き込む
+//@param  :対象アドレス(addr...byte)
+//@param  :データ(data...byte)
+//@return :なし(void)
 void ymz294::setRegister(byte addr,byte data){
+    //アドレス
     _wrcs   = 0;
     _a0     = 0;
     _shiftRegister.setData(addr);
     _wrcs   = 1;
-
+    //データ
     _wrcs   = 0;
     _a0     = 1;
     _shiftRegister.setData(data);
@@ -18,7 +26,9 @@ void ymz294::setRegister(byte addr,byte data){
     return;
 }
 
-//リセット、複数つなぐと使えないので注意
+//@brief  :単一使用時限定のリセット
+//@param  :なし(void)
+//@return :なし(void)
 void ymz294::reset(void){
     for(int i = 0;i < 14;i++){
         setRegister(i,0x00);
@@ -31,7 +41,10 @@ void ymz294::reset(void){
     return;
 }
 
-//周波数セット
+//@brief  :指定周波数にセットする
+//@param  :対象チャンネル(ch...int)
+//@param  :周波数(freq...word)
+//@return :なし(void)
 void ymz294::setFreqency(int ch,word freq){
     word calFreqency = 0;
     if(freq){
@@ -43,7 +56,10 @@ void ymz294::setFreqency(int ch,word freq){
     return;
 }
 
-//音量の変更
+//@brief  :音量の変更
+//@param  :対象チャンネル(ch...int)
+//@param  :音量(vol...int)
+//@return :なし(void)
 void ymz294::setVolume(int ch,int vol){
     if(vol >=0 && vol <= 16){
         ymz294::setRegister((0x08 + ch), vol);
@@ -52,7 +68,9 @@ void ymz294::setVolume(int ch,int vol){
     }
 }
 
-//全レジスタを真っ白に
+//@brief  :全レジスタに0を書き込む、一括リセットの下準備
+//@param  :なし(void)
+//@return :なし(void)
 void ymz294::sendZero(void){
     for(int i = 0;i < 14;i++){
         setRegister(i,0x00);
